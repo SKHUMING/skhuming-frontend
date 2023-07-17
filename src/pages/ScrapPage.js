f61a42105bfd1ae884bdc9aeea9db7cdf87c076e
MyungJiwoo <1206jiwoo@gmail.com>
2023-07-17 18:06:48 +0900

    Feat: 초기 페이지 생성 및 라우터 연결

---
+++ src/pages/ScrapPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    h1 {
+        color: #2d6dcc;
+    }
+`;
+
+function ScrapPage() {
+    return (
+        <Container>
+            <h1>ScrapPage</h1>
+        </Container>
+    );
+}
+
+export default ScrapPage;
