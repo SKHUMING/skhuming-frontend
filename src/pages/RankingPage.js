44b61c324ef7ba08f6cc24e3f889c7aac9b0c1ab
MyungJiwoo <1206jiwoo@gmail.com>
2023-08-27 13:22:32 +0900

    Refactor: 랭킹 페이지에 myRanking 추가

--- src/pages/RankingPage.js
+++ src/pages/RankingPage.js
@@ -3,8 +3,13 @@ import { useState } from "react";
 import styled from "styled-components";
 import MainHeader from "../components/MainHeader.js";
 import RankBox from "../components/RankBox.js";
-
 import axios from "axios";
+
+import tier_SS from "../images/tier_SS.png";
+import tier_S from "../images/tier_S.png";
+import tier_A from "../images/tier_A.png";
+import tier_B from "../images/tier_B.png";
+import tier_Un from "../images/tier_UN.png";
 
 const Container = styled.div`
     display: flex;
@@ -14,6 +19,21 @@ const Container = styled.div`
     .rankingBox {
         width: 45vw;
         margin: 15vh 0;
+    }
+
+    .myRanking {
+        width: 41vw;
+        height: 14vh;
+        padding: 0 2vw;
+        margin-bottom: 20vh;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        border-radius: 0.625rem;
+        background-color: #f6f6f6;
+        box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.05);
     }
 
     .rankingTitle {
@@ -55,7 +75,7 @@ const Container = styled.div`
         background: #2d6dcc;
 
         color: #fff;
-        font-size: 1.5rem;
+        font-size: 1.4rem;
         font-weight: bold;
     }
 
@@ -91,6 +111,13 @@ const Container = styled.div`
         align-items: center;
     }
 
+    .rank_department {
+        margin-top: 0.1rem;
+
+        color: #3a73c9;
+        font-size: 0.75rem;
+    }
+
     .rank_name {
         font-size: 1.3rem;
         color: #204782;
@@ -101,6 +128,21 @@ const Container = styled.div`
         color: #3a73c9;
     }
 `;
+
+function rankImg(tier) {
+    switch (tier) {
+        case "SS":
+            return <img src={tier_SS} alt="tier"></img>;
+        case "S":
+            return <img src={tier_S} alt="tier"></img>;
+        case "A":
+            return <img src={tier_A} alt="tier"></img>;
+        case "B":
+            return <img src={tier_B} alt="tier"></img>;
+        default:
+            return <img src={tier_Un} alt="tier"></img>;
+    }
+}
 
 function RankingPage() {
     // 동일 점수는 같은 등수로 매기는 함수
@@ -114,19 +156,36 @@ function RankingPage() {
     }
 
     const [data, setData] = useState([]);
+    const [myData, setMyData] = useState([]);
     const [loading, setLoading] = useState(false);
     async function getData() {
         try {
             const response = await axios.get(
                 "https://api.skhuming-api.store/api/ranking/list"
             );
-            setData(response.data);
+
+            const modifiedDataList = response.data.map((item) => ({
+                ...item,
+                rank: rankIndex(item.score),
+            }));
+
+            setData(modifiedDataList);
+
+            for (r of modifiedDataList) {
+                // number라서 ==
+                if (r.memberId == window.localStorage.getItem("memberId")) {
+                    setMyData(r);
+                }
+            }
+
             if (data.length > 0) setLoading(true);
             setLoading(true);
         } catch (error) {
             console.error(error);
         }
     }
+
+    console.log(myData);
 
     useEffect(() => {
         getData();
@@ -136,6 +195,34 @@ function RankingPage() {
         <Container>
             <MainHeader />
             <div class="rankingBox">
+                {myData ? (
+                    <>
+                        <div className="rankingTitle">
+                            <p>MY RANKING</p>
+                            <hr />
+                        </div>
+                        <div className="myRanking">
+                            <div className="rank">{myData.rank}</div>
+                            <div className="rank_img">
+                                {rankImg(myData.tier)}
+                            </div>
+                            <div className="rank_user">
+                                <div className="rank_user_profile">
+                                    <div className="rank_name">
+                                        {myData.nickname}
+                                    </div>
+                                    <div className="rank_department">
+                                        {myData.department}
+                                    </div>
+                                </div>
+                                <div className="rank_score">
+                                    {myData.score}점
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                ) : null}
+
                 <div className="rankingTitle">
                     <p>SKHUMING RANKING</p>
                     <hr />
@@ -144,7 +231,7 @@ function RankingPage() {
                 <div className="ranking">
                     {data.map((item) => (
                         <RankBox
-                            rank={rankIndex(item.score)}
+                            rank={item.rank}
                             tier={item.tier}
                             name={item.nickname}
                             department={item.department}
