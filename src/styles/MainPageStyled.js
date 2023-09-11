916779af055312b77433a44704e395de68c913bb
MyungJiwoo <1206jiwoo@gmail.com>
2023-09-11 14:22:26 +0900

    Feat: Footer 추가

--- src/styles/MainPageStyled.js
+++ src/styles/MainPageStyled.js
@@ -338,4 +338,32 @@ export const Container = styled.div`
         background-clip: text; /* 텍스트를 마스킹하여 배경 그라데이션을 표시 */
         animation: ${shine} 2s infinite;
     }
+
+    /* // footer
+    .footer {
+        width: 60vw;
+        padding: 4vh 20vw;
+        position: absolute;
+        left: 0;
+
+        background-color: #efefef;
+        color: #9e9e9e;
+
+        box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.25);
+    }
+
+    .footer__name {
+        font-size: 17px;
+        color: #898989;
+        font-weight: bold;
+    }
+
+    .footer__content > span {
+        color: #898989;
+        font-weight: bold;
+    }
+
+    .footer > hr {
+        width: 60vw;
+    } */
 `;
